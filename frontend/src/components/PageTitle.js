3fc76d5110332f2cf35da3403e88f88387c35ff7
seanyoungblood <122582515+seanyoungblood@users.noreply.github.com>
2023-03-23 19:06:09 -0400

    Update PageTitle.js

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -3,7 +3,7 @@ import React from 'react';
 function PageTitle()
 {
    return(
-     <h1 id="title">Study Buddy 23 THIS IS A TEST</h1>
+     <h1 id="title">Study Buddy 23 THIS IS A TEST WEEEEEEE</h1>
    );
 };
 
