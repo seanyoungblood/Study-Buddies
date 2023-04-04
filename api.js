9538517e18fd64c471389c3a5aceb5720eef9e15
seanyoungblood <122582515+seanyoungblood@users.noreply.github.com>
2023-04-04 12:39:45 -0400

    Update api.js

--- api.js
+++ api.js
@@ -131,7 +131,7 @@ app.delete('/api/deleteUser', async (req, res, next) =>
     const {username} = req.body;
 
     const db = client.db("StudyBuddy");
-    const results = await db.collection('users').deleteOne({"users.username":username}).toArray();
+    db.collection('users').deleteOne({"users.username":username});
   
     var ret = {error:error};
     res.status(200).json(ret);
