7a975c97ad5c42514fe7aa337356571dd8f632b5
Tayl-r <91978020+Tayl-r@users.noreply.github.com>
2023-04-04 14:28:41 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -37,11 +37,27 @@ if (process.env.NODE_ENV === 'production')
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => res.sendFile(
-        path.resolve(__dirname, 'frontend', 'build', 'index.html')
+        //path.resolve(__dirname, 'frontend', 'build', 'index.html')
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
     ))
 } else{
     app.get('/', (req, res) => res.send('Please set to production'))
 }
+
+/*
+THIS CODE WORKS DURING THE TURORIAL HEROKU
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
+*/
 
 
 app.use(errorHandler)
