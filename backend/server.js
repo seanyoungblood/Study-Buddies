2f31baa90db3a2462495114abde97efa3be270b8
Tayl-r <91978020+Tayl-r@users.noreply.github.com>
2023-04-04 15:05:11 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,20 @@ app.use(express.json())
  */
 app.use(express.urlencoded({ extended: false }))
 
+app.use((req, res, next) => 
+{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+
 /**
  * The URL can only be accessed by the '/api/classes'
  */
@@ -37,27 +51,11 @@ if (process.env.NODE_ENV === 'production')
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => res.sendFile(
-        //path.resolve(__dirname, 'frontend', 'build', 'index.html')
         path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
     ))
 } else{
     app.get('/', (req, res) => res.send('Please set to production'))
 }
-
-/*
-THIS CODE WORKS DURING THE TURORIAL HEROKU
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-    )
-  );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
-*/
 
 
 app.use(errorHandler)
