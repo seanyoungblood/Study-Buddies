17d673114024f3b52846d166f43ae18c9aca04ce
seanyoungblood <122582515+seanyoungblood@users.noreply.github.com>
2023-04-11 20:04:13 -0400

    Update api.js

--- api.js
+++ api.js
@@ -19,14 +19,14 @@ app.post('/api/login', async (req, res, next) =>
     const db = client.db("StudyBuddy");
     const results = await db.collection('users').find({username:login,password:password}).toArray();
   
-    var id = -1;
-    var fn = '';
-    var ln = '';
-
-        id = results[0]._id;
+    //var id = -1;
+    var fn = "";
+    var ln = "";
+    if (results.length > 0) {
+        //id = results[0]._id;
         fn = results[0].firstName;
         ln = results[0].lastName;
-
+    }
     var ret = { firstName:fn, lastName:ln, _id:id, error:error };
     res.status(200).json(ret);
 });
