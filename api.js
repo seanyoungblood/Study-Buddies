3034e6bf9aac6965c9850be415e7d3ca2057dcd1
seanyoungblood <122582515+seanyoungblood@users.noreply.github.com>
2023-04-13 18:31:51 -0400

    Update api.js

--- api.js
+++ api.js
@@ -165,7 +165,7 @@ app.post('/api/searchGroups', async (req, res, next) =>
     
       var _search = search.trim();
       
-      const db = client.db();
+      const db = client.db("StudyBuddy");
       const results = await db.collection('users').find({"username":{$regex:_search+'.*', $options:'r'}}).toArray();
       
       var _ret = [];
