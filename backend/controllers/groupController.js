2f45ce818ceb93459f3c1066e987b4ca6232add9
Tayl-r <91978020+Tayl-r@users.noreply.github.com>
2023-04-19 11:07:42 -0400

    Update groupController.js

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -187,7 +187,7 @@ const editRating = asyncHandler(async (req, res) => {
 
     // const db = client.db("StudyBuddy");
     // db.collection('groups').updateOne({groupName:groupName}, { $push: {
-    groupie.updateOne({groupName:groupName}, { $set: {
+    groupie.updateOne({groupName:groupName}, { $push: {
     //groupie.findOneAndUpdate({groupName:groupName}, { $set: {
         "rating":rating
     } })
