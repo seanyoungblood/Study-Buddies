c0bdce5ab6e8ab6080c3a11435995fb422bdbe1e
Tayl-r <91978020+Tayl-r@users.noreply.github.com>
2023-04-19 00:36:31 -0400

    Update groupRoutes.js

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -5,6 +5,7 @@ const {
     searchGroup,
     joinGroup,
     leaveGroup,
+    editGroup,
 } = require('../controllers/groupController')
 const { protect } = require('../middleware/authMiddleware')
 
@@ -14,5 +15,6 @@ router.route('/registerGroup').post(protect, registerGroup)
 router.route('/searchGroup').get(searchGroup)
 router.route('/joinGroup').post(protect, joinGroup)
 router.route('/leaveGroup').post(protect, leaveGroup)
+router.route('/editGroup').put(editGroup)
 
 module.exports = router
