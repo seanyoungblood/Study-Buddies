0aa0b432f202e97f57ca666fede44462cf2624b5
Alexander Green <alexneilgreen@gmail.com>
2023-04-19 21:43:55 -0400

    cssProfile

--- frontend/src/components/ProfileButtons.js
+++ frontend/src/components/ProfileButtons.js
@@ -4,12 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ProfileButtons = () => {
     return ( 
-        <div className="row text-center mt-5">
-            <ul>
-                <li><Link to='/user'>Edit User</Link></li>
-                <li><Link to='/classes'>Change Classes</Link></li>
-                <li><Link to='/'>Delete Account</Link></li>
-            </ul>
+        <div className="buttonRow text-center mt-5">
+            <button id="profileButton"><Link to='/user'>Edit User</Link></button>
+            <button id="profileButton"><Link to='/classes'>Change Classes</Link></button>
+            <button id="profileButton"><Link to='/'>Delete Account</Link></button>
         </div>
      );
 }
