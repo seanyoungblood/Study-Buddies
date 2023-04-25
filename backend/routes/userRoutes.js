6a858eef2c82c6d84c6a110e06e37f8ae09b6225
seanyoungblood <122582515+seanyoungblood@users.noreply.github.com>
2023-04-25 13:35:19 -0400

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, 
+const { nodemail,
+        registerUser, 
         loginUser, 
         getMe,
         searchUser,
@@ -11,6 +12,7 @@ const { registerUser,
       } = require('../controllers/userController')
 const { protect } = require('../middleware/authMiddleware')
 
+router.post('/nodemail', nodemail)
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/me', protect, getMe)
