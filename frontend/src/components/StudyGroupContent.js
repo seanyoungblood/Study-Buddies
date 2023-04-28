ffcb5b6a2cc4005aaf8d789dda9ac47be5576a8b
Alexander Green <alexneilgreen@gmail.com>
2023-04-28 12:50:33 -0400

    New StudyPageContent.js

--- frontend/src/components/StudyGroupContent.js
+++ frontend/src/components/StudyGroupContent.js
@@ -5,40 +5,96 @@ const Content = () => {
     return ( 
         <section className="group-section">
             <div class="search-bar">
-                <input type="text" placeholder="Search"></input>
-                <Link class="create-group" to='/group'>Create Group</Link>
+                <input type="text" placeholder="Search..."></input>
+                <button class="create-group-btn">Create Group</button>
             </div>
 
-            <div id="group-info-box">
-                <div id="group-name">Group Name Example</div>
-                <div class="group-details" className="study-rows">
-                    <div id="group-summary">Group Summary<br></br><br></br>Text goes here Text goes here Text goes here Text goes here Text goes here Text goes here Text goes here Text goes here</div>
-                    <div id="group-classes">Classes<br></br><br></br>COP 4331<br></br>EEL 3123<br></br>MAC 2112<br></br>PSY 8179</div>
+            <div class="tile-container">
+                <div class="tile">
+                <div class="tile-header">
+                    <div id="group-name">Group Name</div>
+                    <div class="header-details">
+                        <p id="group-course">Course: Course Name</p>
+                        <p id="group-date">Date: MM/DD/YYYY</p>
+                        <p id="group-time">Time: HH:MM</p>
+                    </div>
                 </div>
-                <div id="group-rating">
-                    Rating:
-                    <span id="star1" class="star active">&#9733;</span>
-                    <span id="star2" class="star active">&#9733;</span>
-                    <span id="star3" class="star active">&#9733;</span>
-                    <span id="star4" class="star">&#9733;</span>
-                    <span id="star5" class="star">&#9733;</span>
-                    <button class="join-btn">Join</button>
-                    <button class="delete-btn">Delete</button>
+                <div class="tile-content">
+                    <div class="objective">
+                        <p>Objective:</p>
+                        <p id="group-objective">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra ex et mauris finibus, ac blandit orci rutrum.</p>
+                    </div>
+                    <div class="members">
+                        <p>Members:</p>
+                        <ul>
+                        <li>Member 1</li>
+                        <li>Member 2</li>
+                        <li>Member 3</li>
+                        </ul>
+                    </div>
                 </div>
-            </div> 
+                <div class="tile-footer">
+                    <div id="group-rating">
+                        <span>Rating:</span>
+                        <span class="star active">&#9733;</span>
+                        <span class="star active">&#9733;</span>
+                        <span class="star active">&#9733;</span>
+                        <span class="star active">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                    </div>
+                    <div>
+                        <button class="join-btn">Join Group</button>
+                        <button class="review-btn">Leave Review</button>
+                    </div>
+                </div>
+                </div>
 
-            <div class="pagination-bar">
-                {/* <button class="prev-btn">&lt;</button> */}
-                <button class="prev-btn">Prev Group</button>
-                {/* <div class="page-numbers">
-                    <button id="but1">1</button>
-                    <button id="but2">2</button>
-                    <button id="but3">3</button>
-                    <button id="but4">4</button>
-                    <button id="but5">5</button>
-                </div> */}
-                {/* <button class="next-btn">&gt;</button> */}
-                <button class="next-btn">Next Group</button>
+                <div class="tile">
+                    <div class="tile-header">
+                    <div id="group-name">Group Name</div>
+                    <div class="header-details">
+                        <p id="group-course">Course: Course Name</p>
+                        <p id="group-date">Date: MM/DD/YYYY</p>
+                        <p id="group-time">Time: HH:MM</p>
+                    </div>
+                    </div>
+                    <div class="tile-content">
+                    <div class="objective">
+                        <p>Objective:</p>
+                        <p id="group-objective">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam viverra ex et mauris finibus, ac blandit orci rutrum.</p>
+                    </div>
+                    <div class="members">
+                        <p>Members:</p>
+                        <ul>
+                            <li>Member 1</li>
+                            <li>Member 2</li>
+                            <li>Member 3</li>
+                        </ul>
+                    </div>
+                    </div>
+                    <div class="tile-footer">
+                    <div id="group-rating">
+                        <span>Rating:</span>
+                        <span class="star active">&#9733;</span>
+                        <span class="star active">&#9733;</span>
+                        <span class="star active">&#9733;</span>
+                        <span class="star active">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                    </div>
+                    <div>
+                        <button class="join-btn">Join Group</button>
+                        <button class="review-btn">Leave Review</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pagination">
+                <a href="#">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+                <a href="#">4</a>
+                <a href="#">5</a>
             </div>
         </section>
      );
