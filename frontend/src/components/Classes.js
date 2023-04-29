23e364b513281bb78b0f3635071cac52b7bed990
Adam Farrow <adamfarrow1@gmail.com>
2023-04-28 21:35:52 -0400

    update edit

--- frontend/src/components/Classes.js
+++ frontend/src/components/Classes.js
@@ -99,7 +99,7 @@ function buildPath(route)
           <input className='input-field mt-1' type="text" id="class4" placeholder="Your Class Here" ref={(c) => RegisterClass4 = c} /><br />
           <input className='input-field mt-1' type="text" id="class5" placeholder="Your Class Here" ref={(c) => RegisterClass5 = c} /><br />
           <input className='input-field mt-1' type="text" id="class5" placeholder="Your Class Here" ref={(c) => RegisterClass6 = c} /><br />
-          <Link to="/" className='variant1-btn mt-3 edit-user-btn d-block align-center-btn' id="classesButton"  onClick={doClasses}>Change classes</Link>
+          <Link to="/" className='variant1-btn mt-3 edit-user-btn d-block align-center-btn' onClick={doClasses}>Change classes</Link>
         </form>
       <span id="classesResult">{message}</span>
      </div>
