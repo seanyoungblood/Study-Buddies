5128ee8c48849c5ea40bc6956bda3410178f9453
Alexander Green <alexneilgreen@gmail.com>
2023-04-29 18:52:11 -0400

    searchGroup

--- frontend/src/components/StudyGroupContent.js
+++ frontend/src/components/StudyGroupContent.js
@@ -36,38 +36,37 @@ const Content = () => {
         <section className="group-section">
             <div className="search-bar">
                 <input type="text" placeholder="Search..." onChange={(e) => {setQuery(e.target.value);}} />
-                {/*onInput={useEffect}*/}
                 <Link className="create-group-btn" to="/group">Create Group</Link>
             </div>
 
             {/* <div class="tile-container">
-                {data.map((data, index) => (
+                {data.map((value) => (
                     <div class="tile">
                         <div class="tile-header">
-                            <div id="group-name" key={data.groupName}>{data.groupName}</div>
+                            <div id="group-name" key={value.groupName}>{value.groupName}</div>
                             <div class="header-details">
-                                <p id="group-course" key={data.course}>{data.course}</p>
-                                <p id="group-date" key={data.date}>{data.date}</p>
-                                <p id="group-time" key={data.time}>{data.time}</p>
+                                <p id="group-course" key={value.course}>{value.course}</p>
+                                <p id="group-date" key={value.date}>{value.date}</p>
+                                <p id="group-time" key={value.time}>{value.time}</p>
                             </div>
                         </div>
                         <div class="tile-content">
                             <div class="objective">
                                 <p>Objective:</p>
-                                <p id="group-objective" key={data.objective}>{data.objective}</p>
+                                <p id="group-objective" key={value.objective}>{value.objective}</p>
                                 <p>Location:</p>
-                                <p key={data.location}>{data.location}</p>
+                                <p key={value.location}>{value.location}</p>
                             </div>
                             <div class="members">
                                 <p>Members:</p>
                                 <ul>
-                                <li key={data.members}>{data.members}</li>
+                                    <li key={value.members}>{value.members}</li>
                                 </ul>
                             </div>
                         </div>
                         <div class="tile-footer">
                             <div id="group-rating">
-                                <span>Rating:</span>
+                                <span key={value.reviews}>Rating:</span>
                                 <span class="star active">&#9733;</span>
                                 <span class="star active">&#9733;</span>
                                 <span class="star active">&#9733;</span>
