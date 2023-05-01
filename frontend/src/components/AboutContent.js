285e8e862e09ad46d62d90996cd8dcd4561d83d2
Alexander Green <alexneilgreen@gmail.com>
2023-04-30 23:20:19 -0400

    a

--- frontend/src/components/AboutContent.js
+++ frontend/src/components/AboutContent.js
@@ -10,37 +10,37 @@ const AboutContent = () => {
             <div class="member">
                 <h3 class="name">Nick Rolland</h3>
                 <p class="title">Project Manager</p>
-                <p class="description">John is an experienced entrepreneur with a background in finance and management.</p>
+                <p class="description">The React JS-based website was developed under proficient leadership that ensured seamless integration and optimal functionality across all components by a cross-functional team led by the project manager.</p>
             </div>
             <div class="member">
                 <h3 class="name">Adam Farrow</h3>
                 <p class="title">Webpage Frontend Dev</p>
-                <p class="description">Jane is a software engineer with expertise in front-end development and UX design.</p>
+                <p class="description">Implemented API calls into the web design of a React JS-based website as a frontend developer, achieving seamless integration of the website's various components and delivering an optimal user experience.</p>
             </div>
             <div class="member">
                 <h3 class="name">Alexander Green</h3>
                 <p class="title">Webpage Frontend Dev</p>
-                <p class="description">John is an experienced entrepreneur with a background in finance and management.</p>
+                <p class="description">Leveraged expertise in web design to seamlessly integrate the various components of a React JS-based website by incorporating API calls, resulting in enhanced functionality and user experience as a frontend developer.</p>
             </div>
             <div class="member">
                 <h3 class="name">Christian King</h3>
                 <p class="title">Mobile Frontend Dev</p>
-                <p class="description">Jane is a software engineer with expertise in front-end development and UX design.</p>
+                <p class="description">Developed a stunning, seamless, user-friendly, and powerful cross-platform mobile app with awe-inspiring charm and responsive design.</p>
             </div>
             <div class="member">
                 <h3 class="name">Brooke Butner</h3>
                 <p class="title">API Dev</p>
-                <p class="description">John is an experienced entrepreneur with a background in finance and management.</p>
+                <p class="description">Successfully developed and tested API calls for a web application by leveraging expertise in ARC, ensuring optimal functionality and performance of the system.</p>
             </div>
             <div class="member">
                 <h3 class="name">Tayler Mondina</h3>
                 <p class="title">API Dev</p>
-                <p class="description">Jane is a software engineer with expertise in front-end development and UX design.</p>
+                <p class="description">As a member of the API development and testing team, utilized POSTMAN to develop, test, and validate API calls, resulting in a high-performing and functional web application.</p>
             </div>
             <div class="member">
                 <h3 class="name">Sean Youngblood</h3>
                 <p class="title">Backend & Database</p>
-                <p class="description">Jane is a software engineer with expertise in front-end development and UX design.</p>
+                <p class="description">Contributed to the successful deployment and launch of the application by working on hosting, database development, and coding API functions.</p>
             </div>
         </section>
 
