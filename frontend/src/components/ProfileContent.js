c4e34879f67956ad1d3f2d69e77bfb178267e36c
Alexander Green <alexneilgreen@gmail.com>
2023-04-30 20:26:58 -0400

    a

--- frontend/src/components/ProfileContent.js
+++ frontend/src/components/ProfileContent.js
@@ -85,8 +85,7 @@ const ProfileContent = () => {
 
             <div className="col-md-6 col-sm-12 margin-top">
                 <h1 className="textbox profile-header">Groups</h1>
-                {groups?.map((c)=>
-                     (
+                {groups?.map((c)=>(
                         <div onClick={(e) => {handleDelete(e)}} className='profile-classes profile-hover' key={c}>
                             <p>{c}</p>
                         </div>
