1678c9e62cb18b3cb7c34b05bceec1f7665b129d
Adam Farrow <adamfarrow1@gmail.com>
2023-05-02 00:59:50 -0400

    delete group

--- frontend/src/components/ProfileDelete.js
+++ frontend/src/components/ProfileDelete.js
@@ -85,7 +85,7 @@ const ProfileDelete = () => {
     
             try
             {    
-                const response = await fetch(buildPath('api/searchAdmin'),
+                const response = await fetch(buildPath(`api/:${currentUser._id}/searchAdmin`),
                 {method:'DELETE',body:js,headers:{'Content-Type': 'application/json' , 'Authorization': `Bearer ${currentUser.token}`}});
     
                 var res = JSON.parse(await response.text());
